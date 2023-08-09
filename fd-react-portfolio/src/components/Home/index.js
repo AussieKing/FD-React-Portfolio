a1bdc12cbb0289363415e1e331652657e2103efa
Freddy <freddydordoni@Mac-mini.local>
2023-08-09 16:01:27 +1000

    working on home

--- fd-react-portfolio/src/components/Home/index.js
+++ fd-react-portfolio/src/components/Home/index.js
@@ -1,3 +1,20 @@
-const Home = () => ('Hello World!')
+import Logo from '../../assets/images/logo.png'
+import { Link } from 'react-router-dom'
+import './index.scss';
+
+const Home = () => {
+    
+    return (
+        <div classname="container-home">
+            <img src={Logo} alt='dev name' />
+            <div classname="text-area">
+                <h1>Hi, I'm Freddy
+                </h1>
+                <h2>Full Stack Web Developer</h2>
+                <Link to='/contact' classname='flat-button'>Contact Me</Link>
+            </div>
+        </div>
+    )
+}
 
 export default Home
