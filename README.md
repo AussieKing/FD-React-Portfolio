2224f9ac0a2099ee42486632d873e31ec6acfe07
Freddy <freddydordoni@Mac-mini.local>
2023-08-14 23:12:45 +1000

    fixed the positioning of the animation of the writing on all pages

--- README.md
+++ README.md
@@ -16,16 +16,20 @@ WHEN I view the header
 THEN I am presented with the developer's name and navigation with titles corresponding to different sections of the portfolio  []
 
 WHEN I view the navigation titles
-THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding to the current section is highlighted
+THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding to the current section is highlighted []
 
 WHEN I click on a navigation title
-THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted
+THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted []
+
 WHEN I load the portfolio the first time
-THEN the About Me title and section are selected by default
+THEN the About Me title and section are selected by default []
+
 WHEN I am presented with the About Me section
-THEN I see a recent photo or avatar of the developer and a short bio about them
+THEN I see a recent photo or avatar of the developer and a short bio about them  [x]
+
 WHEN I am presented with the Portfolio section
-THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repository
+THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repository  []
+
 WHEN I am presented with the Contact section
 THEN I see a contact form with fields for a name, an email address, and a message
 WHEN I move my cursor out of one of the form fields without entering text
--- fd-react-portfolio/src/components/Portfolio/index.js
+++ fd-react-portfolio/src/components/Portfolio/index.js
@@ -1,7 +1,94 @@
-import React from "react";
+//! PORTFOLIO
+
+import React, { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
+import Navigation from "../Navigation";
 
 const Portfolio = () => {
-    return <div>Hello World</div>
-}
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const [portfolio, setPortfolio] = useState([
+    {
+      title: "Random Password Generator",
+      description: "Generates a random password based on user-selected criteria...",
+      imageUrl: "https://media.giphy.com/media/B34skTNZtR7945bfmE/giphy.gif",
+      projectUrl: "https://github.com/AussieKing/Not-So-Random-Password-Generator",
+    },
+    {
+      title: "To-Do List",
+      description: "This app will run in the browser and feature dynamically updated HTML and CSS powered by JavaScript code...",
+      imageUrl: "https://media.giphy.com/media/5gBb7Kt3KoH37Jtkl6/giphy.gif",  
+      projectUrl: "https://github.com/AussieKing/King-To-Do-List"
+    },
+    {
+      title: "Technology Blog",
+      description: "This blog was created using the MVC paradigm, Handlebars.js, and Sequelize, as a CMS-style blog site...",
+      imageUrl: "https://media.giphy.com/media/qgQGfATzGVE93ADx8D/giphy.gif",  
+      projectUrl: "https://github.com/AussieKing/Techno-MVC"
+    },
+    {
+      title: "Weather Dashboard",
+      description: "This handy app will display the current weather and a 5-day forecast for any city in the world, using API calls...",
+      imageUrl: "https://media.giphy.com/media/qgQGfATzGVE93ADx8D/giphy.gif",  
+      projectUrl: "https://github.com/AussieKing/Open-Weather-App"
+    },
+    {
+      title: "Workanize SQL Database",
+      description: "This command-line application allows the user to view and manage the departments, roles, and employees in a company...",
+      imageUrl: "./assets/images/workanize.png",  
+      projectUrl: "https://github.com/AussieKing/Workanize-SQL"
+    },
+    {
+      title: "Note Taker",
+      description: "This Heroku deployed app allows the user to write and save notes, using Express.js back end and will save and retrieve note data from a JSON file...",
+      imageUrl: "https://media.giphy.com/media/qgQGfATzGVE93ADx8D/giphy.gif",  
+      projectUrl: "https://github.com/AussieKing/Note-Taker-aka-ADHD-Savior"
+    },
+  ]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  return (
+  <>
+  <Navigation />
+    <div className="portfolio-container">
+      <div className="portfolio-header">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={"Portfolio".split("")}
+            idx={15}
+          />
+        </h1>
+      </div>
+      <div className="portfolio-projects">
+        {portfolio.map((project, index) => (
+          <div className="project-box" key={index}>
+            <img src={project.imageUrl} alt={project.title} className="portfolio-image" />
+            <div className="content">
+              <h3 className="title">{project.title}</h3>
+              <p className="description">{project.description}</p>
+              <a href={project.projectUrl} className="btn" target="_blank" rel="noreferrer">
+                View Project
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Loader type="pacman" />
+    </div>
+    </>
+  );
+};
 
 export default Portfolio;
+
